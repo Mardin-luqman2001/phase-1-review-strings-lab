60986297183a742e759c03a1c060aa9aa6e1b13d
Mardin <mardinomer001@gmail.com>
2024-02-20 12:10:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+module.exports = { welcomeMessage, excitedWelcomeMessage, shortGreeting };
